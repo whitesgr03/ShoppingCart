668189e6164a298fdc1d57cf066c326848a48a6c
Weiss Bai <whitesgr03@gmail.com>
2023-06-05 15:47:59 +0800

    Add Product component

---
+++ src/components/Products.jsx
@@ -0,0 +1,14 @@
+import Icon from "@mdi/react";
+import { mdiMagnify } from "@mdi/js";
+
+const Products = () => {
+	return (
+		<div>
+			<p>This is Products page!</p>
+			<Icon path={mdiMagnify} size={1} />
+		</div>
+
+	);
+};
+
+export default Products;
