b66dfadaadafa53f729778a43092ac994680312c
Weiss Bai <whitesgr03@gmail.com>
2023-06-05 15:41:06 +0800

    Add Error component

--- src/RouteSwitch.jsx
+++ src/RouteSwitch.jsx
@@ -1,10 +1,12 @@
 import Root from "./components/Root";
+import Error from "./components/Error";
 import { createBrowserRouter } from "react-router-dom";
 
 const RouteSwitch = createBrowserRouter([
 	{
 		path: "/",
 		element: <Root />,
+		errorElement: <Error />,
 	},
 ]);
 
