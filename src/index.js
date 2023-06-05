ac14e9880aec90d58788dbcdf9a5c54f92637b87
Weiss Bai <whitesgr03@gmail.com>
2023-06-05 15:29:10 +0800

    Add React Router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+
+import RouteSwitch from "./RouteSwitch";
 import "./style/index.css";
-import Home from "./components/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<Home />
+		<RouterProvider router={RouteSwitch} />
 	</React.StrictMode>
 );
