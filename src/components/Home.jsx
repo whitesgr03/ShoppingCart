894a94afa093edafe74d2ab718a95182aac9bdbe
Weiss Bai <whitesgr03@gmail.com>
2023-06-08 10:12:21 +0800

    Add Home Component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,20 @@
-function Home() {
-	return <div>This home page!</div>;
-}
+const Home = () => {
+	return (
+		<div className="home">
+			<div className="carousel">
+				<button type="button" className="arrow left"></button>
+				<div className="content">
+					<div className="container">
+						<h2 className="title">Casual Summer Collection</h2>
+						<button type="button">VIEW MORE</button>
+					</div>
+				</div>
+				<button type="button" className="arrow right"></button>
+			</div>
+			<div className="info"></div>
+			<div className="category"></div>
+		</div>
+	);
+};
 
-export default Home;
+export { Home as default };
