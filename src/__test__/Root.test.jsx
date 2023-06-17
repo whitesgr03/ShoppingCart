50ee1b7549e2b4f43468e9027f27e6c0abd44ce9
Weiss Bai <whitesgr03@gmail.com>
2023-06-17 14:54:18 +0800

    Update Root test

--- src/__test__/Root.test.jsx
+++ src/__test__/Root.test.jsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import { RouterProvider, createMemoryRouter } from "react-router-dom";
-import "@testing-library/jest-dom";
 
 import Root from "../components/Root";
 
