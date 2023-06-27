ab8278c903e4d07c7514f89aeae2ecfbb675c5a6
Weiss Bai <whitesgr03@gmail.com>
2023-06-27 12:35:24 +0800

    Add icon attribute

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,20 +4,42 @@ import { mdiGithub } from "@mdi/js";
 const Footer = () => {
 	return (
 		<footer>
-			<a
-				href="https://github.com/whitesgr03/ShoppingCart"
-				target="_blank"
-				rel="noreferrer"
-			>
-				&copy; 2023 - GentSkin
-			</a>
-			<a
-				href="https://github.com/whitesgr03/ShoppingCart"
-				target="_blank"
-				rel="noreferrer"
-			>
-				<Icon path={mdiGithub}></Icon>
-			</a>
+			<div>
+				<a
+					href="https://github.com/whitesgr03/ShoppingCart"
+					target="_blank"
+					rel="noreferrer"
+				>
+					&copy; 2023 - GentSkin
+				</a>
+				<a
+					href="https://github.com/whitesgr03/ShoppingCart"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<Icon path={mdiGithub}></Icon>
+				</a>
+			</div>
+			<div className="icon">
+				Icons made by
+				<a
+					href="https://www.flaticon.com/authors/pixel-perfect"
+					title="Pixel perfect"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Pixel perfect
+				</a>
+				<span> from </span>
+				<a
+					href="https://www.flaticon.com/"
+					title="Flaticon"
+					target="_blank"
+					rel="noreferrer"
+				>
+					www.flaticon.com
+				</a>
+			</div>
 		</footer>
 	);
 };
