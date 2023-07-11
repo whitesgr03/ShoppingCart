8fe985960c8f18b034a78b74bf1fc75e364207e1
Weiss Bai <whitesgr03@gmail.com>
2023-07-11 14:07:28 +0800

    Create the productNavbar component
    separate from the store component

---
+++ src/components/products/ProductsNavbar.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const ProductsNavbar = () => {
+	return (
+		<ul className="category">
+			<li>
+				<Link to="/shop">Shop</Link>
+			</li>
+			<li> / </li>
+			<li>
+				<Link to="/shop">Category</Link>
+			</li>
+		</ul>
+	);
+};
+
+export default ProductsNavbar;
