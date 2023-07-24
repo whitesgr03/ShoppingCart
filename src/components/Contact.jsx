a20635d8c4167a4d8c4ad738689c90a9d93a9d48
Weiss Bai <whitesgr03@gmail.com>
2023-07-24 12:48:30 +0800

    Add useOutletContext to receive backgroundImage

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,54 +1,64 @@
+import { useOutletContext } from "react-router-dom";
+
 import Icon from "@mdi/react";
 import { mdiPhone, mdiEmailOutline, mdiMapMarker } from "@mdi/js";
 
 const Contact = () => {
+	const { backgroundImage } = useOutletContext();
+
 	return (
 		<div className="contact">
-			<div>
-				<h2 className="title">Contact Us</h2>
-				<fieldset className="container">
-					<legend>Get in touch</legend>
-					<div className="info">
-						<ul className="icons">
-							<li>
-								<Icon path={mdiPhone} size={1} />
-							</li>
-							<li>
-								<Icon path={mdiEmailOutline} size={1} />
-							</li>
-							<li>
-								<Icon path={mdiMapMarker} size={1} />
-							</li>
-						</ul>
-						<ul className="describe">
-							<li>+123-456-789</li>
-							<li>gentskin@example.com</li>
-							<li>123 Anywhere St.,Any City</li>
-						</ul>
+			<div
+				style={{
+					backgroundImage: backgroundImage.contact,
+				}}
+			>
+				<div className="blur">
+					<h2 className="title">Contact Us</h2>
+					<fieldset className="container">
+						<legend>Get in touch</legend>
+						<div className="info">
+							<ul className="icons">
+								<li>
+									<Icon path={mdiPhone} size={1} />
+								</li>
+								<li>
+									<Icon path={mdiEmailOutline} size={1} />
+								</li>
+								<li>
+									<Icon path={mdiMapMarker} size={1} />
+								</li>
+							</ul>
+							<ul className="describe">
+								<li>+123-456-789</li>
+								<li>gentskin@example.com</li>
+								<li>123 Anywhere St.,Any City</li>
+							</ul>
+						</div>
+						<form id="contact" onSubmit={e => e.preventDefault()}>
+							<label htmlFor="name">
+								Name
+								<input type="text" name="name" id="name" />
+							</label>
+							<label htmlFor="email">
+								Email
+								<input type="text" name="email" id="email" />
+							</label>
+							<label htmlFor="content">
+								Content
+								<textarea
+									name="content"
+									id="content"
+									rows="5"
+								></textarea>
+							</label>
+						</form>
+					</fieldset>
+					<div className="wrap">
+						<button type="submit" form="contact" className="slide">
+							Send Message
+						</button>
 					</div>
-					<form id="contact" onSubmit={e => e.preventDefault()}>
-						<label htmlFor="name">
-							Name
-							<input type="text" name="name" id="name" />
-						</label>
-						<label htmlFor="email">
-							Email
-							<input type="text" name="email" id="email" />
-						</label>
-						<label htmlFor="content">
-							Content
-							<textarea
-								name="content"
-								id="content"
-								rows="5"
-							></textarea>
-						</label>
-					</form>
-				</fieldset>
-				<div className="wrap">
-					<button type="submit" form="contact" className="slide">
-						Send Message
-					</button>
 				</div>
 			</div>
 		</div>
