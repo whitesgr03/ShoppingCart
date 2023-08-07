0345cde791aa3c956011b325c72ef7f0d6cfab73
Weiss Bai <whitesgr03@gmail.com>
2023-08-07 20:18:43 +0800

    Add handleUserAccount

---
+++ src/utils/handleUserAccount.js
@@ -0,0 +1,72 @@
+import { initialFirestore, initialAuth } from "../firebase-config";
+
+const googleLogin = () => {
+	const auth = initialAuth();
+	const provider = new initialAuth.GoogleAuthProvider();
+	auth.signInWithRedirect(provider);
+};
+
+const userLogout = async () => {
+	const auth = initialAuth();
+	try {
+		await auth.signOut();
+		return {
+			message: "User logout success.",
+			success: true,
+		};
+	} catch (error) {
+		return {
+			message: error,
+			success: false,
+		};
+	}
+};
+
+const checkUser = async userId => {
+	const firestore = initialFirestore();
+	const userRef = firestore.collection("users").doc(userId);
+
+	try {
+		const user = await userRef.get();
+		return {
+			message: "Check user success.",
+			success: true,
+			data: {
+				userExists: user.exists,
+			},
+		};
+	} catch (error) {
+		return {
+			message: error,
+			success: false,
+		};
+	}
+};
+
+const createUser = async user => {
+	const { displayName: name, uid, email } = user;
+
+	const userInfo = {
+		name,
+		email,
+	};
+
+	const firestore = initialFirestore();
+
+	const userRef = firestore.collection("users").doc(uid);
+
+	try {
+		await userRef.set(userInfo);
+		return {
+			message: "Create user success.",
+			success: true,
+		};
+	} catch (error) {
+		return {
+			message: error,
+			success: false,
+		};
+	}
+};
+
+export { userLogout, googleLogin, checkUser, createUser };
