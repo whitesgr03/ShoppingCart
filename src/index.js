e6733cf42c1d3d5d5a2dbb1665169691c9a177bd
Weiss Bai <whitesgr03@gmail.com>
2023-11-08 15:54:28 +0800

    Remove RouterProvider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
 
 import RouteSwitch from "./RouteSwitch";
 import "./style/index.css";
@@ -8,6 +7,6 @@ import "./style/index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<RouterProvider router={RouteSwitch} />
+		<RouteSwitch />
 	</React.StrictMode>
 );
