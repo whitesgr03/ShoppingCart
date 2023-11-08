5d2978a0c482d0d50693d36484d6987a6057ec9b
Weiss Bai <whitesgr03@gmail.com>
2023-11-08 15:53:59 +0800

    Create RouteSwitch and return RouterProvider

--- src/RouteSwitch.jsx
+++ src/RouteSwitch.jsx
@@ -1,56 +1,54 @@
 import Root from "./App/Root";
-
 import Home from "./components/Home";
-
 import ShopPage from "./components/products/ShopPage";
 import ProductList from "./components/products/ProductList";
 import SingleProductPage from "./components/products/SingleProductPage";
-
 import Contact from "./components/Contact";
-
 import AuthGuard from "./App/AuthGuard";
-
 import Error from "./App/Error";
 
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const RouteSwitch = createBrowserRouter([
-	{
-		path: "/",
-		element: <Root />,
-		children: [
-			{
-				index: true,
-				element: <Home />,
-			},
-			{
-				path: "shop",
-				element: <ShopPage />,
-				children: [
-					{
-						index: true,
-						element: <ProductList />,
-					},
-					{
-						path: ":productId",
-						element: (
-							<AuthGuard>
-								<SingleProductPage />,
-							</AuthGuard>
-						),
-					},
-				],
-			},
-			{
-				path: "contact",
-				element: <Contact />,
-			},
-			{
-				path: "*",
-				element: <Error />,
-			},
-		],
-	},
-]);
+const RouteSwitch = () => {
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Root />,
+			children: [
+				{
+					path: "*",
+					element: <Error />,
+				},
+				{
+					index: true,
+					element: <Home />,
+				},
+				{
+					path: "shop",
+					element: <ShopPage />,
+					children: [
+						{
+							index: true,
+							element: <ProductList />,
+						},
+						{
+							path: ":productId",
+							element: (
+								<AuthGuard>
+									<SingleProductPage />,
+								</AuthGuard>
+							),
+						},
+					],
+				},
+				{
+					path: "contact",
+					element: <Contact />,
+				},
+			],
+		},
+	]);
+	return <RouterProvider router={router} />;
+};
 
 export default RouteSwitch;
