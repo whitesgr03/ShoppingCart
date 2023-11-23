7021c3d661f607cb6f603bda873933adb854b34f
Weiss Bai <whitesgr03@gmail.com>
2023-11-23 19:13:06 +0800

    Import initialAuth

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,6 +13,8 @@ import Error from "./Error";
 import Loading from "./Loading";
 
 
+import { initialAuth } from "../firebase-config";
+
 import preLoadImage from "../utils/handlePreLoadImage";
 import getStorageImage from "../utils/handleStorageImage";
 import { getUserCart } from "../utils/handleUserCart";
