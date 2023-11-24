9ba441ef1e384f2894351e600f96a0bc90b77561
Weiss Bai <whitesgr03@gmail.com>
2023-11-24 14:43:48 +0800

    Refactor product components
    Added useSearchParams to get user search terms
    
    Add useEffect to obtain all products and use handlePreLoadImage to preload all product images.
    
    Display loading component when getting product
    
    Display Error component when catching error
    
    Display content when getting product is successful

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,42 +1,76 @@
 import "../../style/products/products.css";
-import Icon from "@mdi/react";
-import { mdiLoading } from "@mdi/js";
 
-import { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Outlet, useSearchParams, useOutletContext } from "react-router-dom";
 
 import ProductsNavbar from "./ProductsNavbar";
 import Error from "../Error";
+import Loading from "../Loading";
 
-import useFetchProducts from "../../hook/useFetchProducts";
+import { initialFirestore } from "../../firebase-config";
+import handlePreLoadImage from "../../utils/handlePreLoadImage";
 
 const Products = () => {
-	const { products, error, loading } = useFetchProducts();
+	const [error, setError] = useState(null);
+	const [products, setProducts] = useState(null);
+	const [searchParams, setSearchParams] = useSearchParams();
 
-	const [filterText, setFilterText] = useState("");
+	const { userId, cart, onOpenModule, onGetUserCart } = useOutletContext();
+
+	useEffect(() => {
+		let ignore = false;
+		const firestore = initialFirestore();
+		const productRef = firestore.collection("products");
+
+		const handleGetProducts = async () => {
+			try {
+				const result = await productRef.get();
+
+				await Promise.all(
+					result.docs.map(item => {
+						const obj = { ...item.data() };
+						return handlePreLoadImage(obj.url);
+					})
+				);
+
+				const productsResult = result.docs.map(item => ({
+					id: item.id,
+					...item.data(),
+				}));
+
+				!ignore && setProducts(productsResult);
+			} catch (error) {
+				console.error(error);
+				setError("Service temporarily unavailable");
+			}
+		};
+		handleGetProducts();
+
+		return () => {
+			ignore = true;
+		};
+	}, []);
 
 	return (
-		<div className="shop">
-			<div className="navigation">
-				<ProductsNavbar onFilterText={setFilterText} />
-			</div>
-			<>
-				{loading && (
-					<div data-testid="loading" className="loading">
-						<Icon path={mdiLoading} spin={1} size={3} />
-						Loading...
-					</div>
-				)}
-				{error && <Error message={error} />}
-				{!loading && !error && (
+		<div className="products">
+			{!products && !error && <Loading />}
+			{error && <Error message={error} />}
+			{products && !error && (
+				<>
+					<ProductsNavbar onSearchParams={setSearchParams} />
 					<Outlet
 						context={{
 							products,
-							filterText,
+							searchParams,
+							userId,
+							cart,
+							onOpenModule,
+							onGetUserCart,
+							onError: setError,
 						}}
 					/>
-				)}
-			</>
+				</>
+			)}
 		</div>
 	);
 };
