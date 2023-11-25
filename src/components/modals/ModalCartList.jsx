19be7f77e438f02c757c7fd5252deb6f27254541
Weiss Bai <whitesgr03@gmail.com>
2023-11-25 17:52:33 +0800

    Add memo toModalCartList component

--- src/components/modals/ModalCartList.jsx
+++ src/components/modals/ModalCartList.jsx
@@ -20,94 +20,87 @@ const Options = memo(quantity =>
 		))
 );
 
+const ModalCartList = memo(
+	({ cart, userId, onError, onGetUserCart, onOpenModule }) => {
+		const [loading, setLoading] = useState(false);
 
-	const handleRemove = product => onOpenModule("alert", product, "remove");
-	const handleChange = async product => {
-		try {
-			setLoading(true);
-			await updateUserCartItem(product, userId);
-			await onGetUserCart(userId);
-		} catch (error) {
-			console.error(error);
-			onError("Service temporarily unavailable");
-		} finally {
-			setLoading(false);
-		}
-	};
+		const handleRemove = product =>
+			onOpenModule("alert", product, "remove");
 
-	return (
-		<div className="items">
-			<div className="itemWarp">
-				{cart.map(product => (
-					<div className="item" key={product.id}>
-						<div className="warp">
-							<img src={product.url} alt={product.name} />
-						</div>
-						<div className="description">
-							<div className="info">
-								<div className="name">{product.name}</div>
-								<div className="price">
-									${product.price.toFixed(2)}
+		const handleChange = async product => {
+			try {
+				setLoading(true);
+				await updateUserCartItem(product, userId);
+				await onGetUserCart(userId);
+			} catch (error) {
+				console.error(error);
+				onError("Service temporarily unavailable");
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		return (
+			<div className="items">
+				<div className="itemWarp">
+					{cart.map(product => (
+						<div className="item" key={product.id}>
+							<div className="warp">
+								<img src={product.url} alt={product.name} />
+							</div>
+							<div className="description">
+								<div className="info">
+									<div className="name">{product.name}</div>
+									<div className="price">
+										${product.price.toFixed(2)}
+									</div>
+								</div>
+
+								<div className="subtotal">
+									<label htmlFor={product.name}>
+										Quantity
+										<select
+											data-testid="quantity"
+											name="quantity"
+											id={product.name}
+											onChange={e =>
+												handleChange({
+													...product,
+													quantity: +e.target.value,
+												})
+											}
+											value={product.quantity}
+										>
+										</select>
+									</label>
+									<p>
+										Subtotal: $
+										{(
+											product.quantity * product.price
+										).toFixed(2)}
+									</p>
 								</div>
 							</div>
-
-							<div className="subtotal">
-								<label htmlFor={product.name}>
-									Quantity
-									<select
-										data-testid="quantity"
-										name="quantity"
-										id={product.name}
-										onChange={e =>
-											handleChange({
-												...product,
-												quantity: +e.target.value,
-											})
-										}
-										value={product.quantity}
-									>
-										{Array.from({
-											length:
-												product.quantity > 10
-													? product.quantity
-													: 10,
-										})
-											.fill([])
-											.map((_, i) => (
-												<option key={i} value={i + 1}>
-													{i + 1}
-												</option>
-											))}
-									</select>
-								</label>
-								<p>
-									Subtotal: $
-									{(product.quantity * product.price).toFixed(
-										2
-									)}
-								</p>
-							</div>
+							<button
+								className="removeBtn"
+								data-testid="removeBtn"
+								onClick={() => handleRemove(product)}
+							>
+								<Icon path={mdiTrashCanOutline} />
+							</button>
 						</div>
-						<button
-							className="removeBtn"
-							data-testid="removeBtn"
-							onClick={() => handleRemove(product)}
-						>
-							<Icon path={mdiTrashCanOutline} />
-						</button>
-					</div>
-				))}
-			</div>
-
-			{loading && (
-				<div className="loading">
-					<Icon path={mdiLoading} spin={1} size={3}></Icon>
+					))}
 				</div>
-			)}
-		</div>
-	);
-};
 
+				{loading && (
+					<div className="loading">
+						<Icon path={mdiLoading} spin={1} size={3}></Icon>
+					</div>
+				)}
+			</div>
+		);
+	}
+);
 ModalCartList.propTypes = {
 	cart: PropTypes.array,
 	userId: PropTypes.string,
