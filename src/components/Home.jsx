17afde6ae087ab96fed4e0e42bc7b95d74f9f875
Weiss Bai <whitesgr03@gmail.com>
2023-11-26 21:30:30 +0800

    Add useContext instead useOutletContext

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import { AppContext } from "./App";
 
 const Home = () => {
-	const { homeUrl: url } = useOutletContext();
+	const { homeUrl: url } = useContext(AppContext);
 
 	return (
 		<div className="home">
