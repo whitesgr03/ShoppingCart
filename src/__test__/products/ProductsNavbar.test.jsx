070382613541eaa56e6cfc9f4b2ca4c29abe56ff
Weiss Bai <whitesgr03@gmail.com>
2023-11-27 09:21:28 +0800

    Refactor ProductNavbar.test

--- src/__test__/products/ProductsNavbar.test.jsx
+++ src/__test__/products/ProductsNavbar.test.jsx
@@ -1,23 +1,58 @@
-import { render } from "@testing-library/react";
-import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { BrowserRouter } from "react-router-dom";
 
 import ProductsNavbar from "../../components/products/ProductsNavbar";
 
-describe("Renders ProductsNavbar Component", () => {
-	it("Should return ProductsNavbar DOM", () => {
-		const routes = [
-			{
-				path: "/",
-				element: <ProductsNavbar />,
-			},
-		];
+describe("ProductsNavbar Component", () => {
+	it("Should add class when input element gets focus", async () => {
+		render(<ProductsNavbar />, { wrapper: BrowserRouter });
 
-		const router = createMemoryRouter(routes, {
-			initialEntries: ["/"],
+		const input = screen.getByRole("searchbox");
+		const searchBar = screen.getByTestId("searchBar");
+
+		await waitFor(() => {
+			input.focus();
 		});
 
-		const { container } = render(<RouterProvider router={router} />);
+		expect(searchBar).toHaveClass("active-border");
+	});
+	it("Should remove class when input element become blur", async () => {
+		render(<ProductsNavbar />, { wrapper: BrowserRouter });
 
-		expect(container).toMatchSnapshot();
+		const input = screen.getByRole("searchbox");
+		const searchBar = screen.getByTestId("searchBar");
+
+		await waitFor(() => {
+			input.blur();
+		});
+
+		expect(searchBar).not.toHaveClass("active-border");
+	});
+	it("Should set value when input element is typed", async () => {
+		const user = userEvent.setup();
+
+		render(<ProductsNavbar />, { wrapper: BrowserRouter });
+
+		const input = screen.getByRole("searchbox");
+
+		await user.type(input, "hello");
+
+		expect(input).toHaveValue("hello");
+	});
+	it("Should correctly handles submit", async () => {
+		const mockSearchParams = jest.fn();
+
+		const user = userEvent.setup();
+
+		render(<ProductsNavbar onSearchParams={mockSearchParams} />, {
+			wrapper: BrowserRouter,
+		});
+
+		const searchButton = screen.getByRole("button");
+
+		await user.click(searchButton);
+
+		expect(mockSearchParams).toBeCalledTimes(1);
 	});
 });
