3266fd42234689b42fdec1d9cd43bf0f5350c6ff
Weiss Bai <whitesgr03@gmail.com>
2023-11-28 14:28:27 +0800

    Refactor JSX
    When product is false, the Loading component is rendered,
    otherwise the content is rendered.

--- src/components/products/ProductDetails.jsx
+++ src/components/products/ProductDetails.jsx
@@ -95,7 +95,34 @@ const ProductDetails = () => {
 	}, [productId, onError]);
 
 	return (
+		<div className="productDetails">
+			{!product && <Loading />}
+			{product && (
+				<>
+					<div className="image">
+						<img src={product.url} alt={product.name} />
+					</div>
+					<div className="info">
+						<div className="description">
+							<h2 className="title">{product.name}</h2>
+							<p>
+								Lorem ipsum dolor sit amet consectetur
+								adipisicing elit. Magnam itaque nisi ipsa
+								pariatur perspiciatis nemo.
+							</p>
+							<p className="price">${product.price.toFixed(2)}</p>
+						</div>
+						<form onSubmit={handleSubmit}>
+							<label htmlFor="quantity">
+								Quantity
+								<select
+									name="quantity"
+									id="quantity"
+									defaultValue={1}
+								>
 									{options}
+								</select>
+							</label>
 
 							<button
 								className={`slide ${adding ? "adding" : ""}`}
@@ -110,6 +137,11 @@ const ProductDetails = () => {
 									<>Add to Cart</>
 								)}
 							</button>
+						</form>
+					</div>
+				</>
+			)}
+		</div>
 	);
 };
 
