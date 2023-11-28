95175222f767797e545501a2bc8c11cb7e49cad4
Weiss Bai <whitesgr03@gmail.com>
2023-11-28 14:23:43 +0800

    Create variable assign options

--- src/components/products/ProductDetails.jsx
+++ src/components/products/ProductDetails.jsx
@@ -23,10 +23,13 @@ const ProductDetails = () => {
 
 	const { productId } = useParams();
 
-	const product = useMemo(
-		() => products.find(item => item.id === productId),
-		[products, productId]
-	);
+	const options = Array.from({ length: OptionNum })
+		.fill([])
+		.map((_, i) => (
+			<option key={i} value={i + 1}>
+				{i + 1}
+			</option>
+		));
 
 	const handleSubmit = e => {
 		const addProduct = async () => {
@@ -90,38 +93,7 @@ const ProductDetails = () => {
 	}, [productId, onError]);
 
 	return (
-		product && (
-			<div className="productDetails">
-				<div className="image">
-					<img src={product.url} alt={product.name} />
-				</div>
-				<div className="info">
-					<div className="description">
-						<h2 className="title">{product.name}</h2>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing
-							elit. Magnam itaque nisi ipsa pariatur perspiciatis
-							nemo.
-						</p>
-						<p className="price">${product.price.toFixed(2)}</p>
-					</div>
-					<form onSubmit={handleSubmit}>
-						<label htmlFor="quantity">
-							Quantity
-							<select
-								name="quantity"
-								id="quantity"
-								defaultValue={1}
-							>
-								{Array.from({ length: OptionNum })
-									.fill([])
-									.map((_, i) => (
-										<option key={i} value={i + 1}>
-											{i + 1}
-										</option>
-									))}
-							</select>
-						</label>
+									{options}
 
 							<button
 								className={`slide ${adding ? "adding" : ""}`}
