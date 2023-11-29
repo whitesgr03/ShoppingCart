b1d64cee0494ffd794c9dbca8f28026f0fb17b0f
Weiss Bai <whitesgr03@gmail.com>
2023-11-29 12:32:55 +0800

    Add JSX markup
    Display the Loading component when fetching the background image,
    otherwise display the content.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -55,59 +55,71 @@ const Contact = () => {
 
 	return (
 		<div className="contact">
-			<div
-				style={{
-					backgroundImage: `url(${url})`,
-				}}
-			>
-				<div className="blur">
-					<h2 className="title">Contact Us</h2>
-					<fieldset className="container">
-						<legend>Get in touch</legend>
-						<div className="info">
-							<ul className="icons">
-								<li>
-									<Icon path={mdiPhone} size={1} />
-								</li>
-								<li>
-									<Icon path={mdiEmailOutline} size={1} />
-								</li>
-								<li>
-									<Icon path={mdiMapMarker} size={1} />
-								</li>
-							</ul>
-							<ul className="describe">
-								<li>+123-456-789</li>
-								<li>gentskin@example.com</li>
-								<li>123 Anywhere St.,Any City</li>
-							</ul>
+			{!url && <Loading />}
+			{url && (
+				<div
+					data-testid="backgroundImage"
+					style={{
+						backgroundImage: `url(${url})`,
+					}}
+				>
+					<div className="blur">
+						<h2 className="title">Contact Us</h2>
+						<fieldset className="container">
+							<legend>Get in touch</legend>
+							<div className="info">
+								<ul className="icons">
+									<li>
+										<Icon path={mdiPhone} size={1} />
+									</li>
+									<li>
+										<Icon path={mdiEmailOutline} size={1} />
+									</li>
+									<li>
+										<Icon path={mdiMapMarker} size={1} />
+									</li>
+								</ul>
+								<ul className="describe">
+									<li>+123-456-789</li>
+									<li>gentskin@example.com</li>
+									<li>123 Anywhere St.,Any City</li>
+								</ul>
+							</div>
+							<form id="contact" onSubmit={handleSubmit}>
+								<label htmlFor="name">
+									Name
+									<input type="text" name="name" id="name" />
+								</label>
+								<label htmlFor="email">
+									Email
+									<input
+										type="text"
+										name="email"
+										id="email"
+									/>
+								</label>
+								<label htmlFor="content">
+									Content
+									<textarea
+										name="content"
+										id="content"
+										rows="5"
+									></textarea>
+								</label>
+							</form>
+						</fieldset>
+						<div className="wrap">
+							<button
+								type="submit"
+								form="contact"
+								className="slide"
+							>
+								Send Message
+							</button>
 						</div>
-						<form id="contact" onSubmit={handleSubmit}>
-							<label htmlFor="name">
-								Name
-								<input type="text" name="name" id="name" />
-							</label>
-							<label htmlFor="email">
-								Email
-								<input type="text" name="email" id="email" />
-							</label>
-							<label htmlFor="content">
-								Content
-								<textarea
-									name="content"
-									id="content"
-									rows="5"
-								></textarea>
-							</label>
-						</form>
-					</fieldset>
-					<div className="wrap">
-						<button type="submit" form="contact" className="slide">
-							Send Message
-						</button>
 					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	);
 };
