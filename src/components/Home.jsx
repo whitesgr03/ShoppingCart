35f16a8ad5a281626a014e9e853e7982c9b58919
Weiss Bai <whitesgr03@gmail.com>
2023-11-29 12:31:19 +0800

    Add JSX markup
    Display the Loading component when fetching the background image,
    otherwise display the content.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -42,23 +42,26 @@ const Home = () => {
 
 	return (
 		<div className="home">
-			<div
-				className="carousel"
-				style={{
-					backgroundImage: `url(${url})`,
-				}}
-			>
-				<button type="button" className="arrow left"></button>
-				<div className="content">
-					<div className="container">
-						<h2 className="title">Casual Summer Collection</h2>
-						<Link className="link" to={`/shop`}>
-							VIEW MORE
-						</Link>
+			{!url && <Loading />}
+			{url && (
+				<div
+					className="carousel"
+					style={{
+						backgroundImage: `url(${url})`,
+					}}
+				>
+					<button type="button" className="arrow left"></button>
+					<div className="content">
+						<div className="container">
+							<h2 className="title">Casual Summer Collection</h2>
+							<Link className="link" to={`/shop`}>
+								VIEW MORE
+							</Link>
+						</div>
 					</div>
+					<button type="button" className="arrow right"></button>
 				</div>
-				<button type="button" className="arrow right"></button>
-			</div>
+			)}
 		</div>
 	);
 };
