ee5ca9f1a3eb3c033f03249d715cf478169b773e
Weiss Bai <whitesgr03@gmail.com>
2023-11-29 14:41:27 +0800

    Import waitFor

--- src/__test__/products/Products.test.jsx
+++ src/__test__/products/Products.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 
 import Products from "../../components/products/Products";
