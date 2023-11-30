0b791038bb9f2f2a4ac14a5ef1a84ca66a904dea
Weiss Bai <whitesgr03@gmail.com>
2023-11-30 09:37:28 +0800

    Change test name

--- src/__test__/Footer.test.jsx
+++ src/__test__/Footer.test.jsx
@@ -2,8 +2,8 @@ import { render } from "@testing-library/react";
 
 import Footer from "../components/layout/Footer";
 
-describe("Renders Footer Component", () => {
-	it("Should return Footer DOM", () => {
+describe("Footer component", () => {
+	it("Should render Footer component", () => {
 		const { container } = render(<Footer />);
 
 		expect(container).toMatchSnapshot();
