664b51ab2057bc08d243df6a4754de124fa18866
Weiss Bai <whitesgr03@gmail.com>
2023-11-30 17:01:07 +0800

    Refactor Options component

--- src/components/modals/ModalCartList.jsx
+++ src/components/modals/ModalCartList.jsx
@@ -8,17 +8,14 @@ import { updateUserCartItem } from "../../utils/handleUserCart";
 
 import PropTypes from "prop-types";
 
-const Options = memo(quantity =>
-	Array.from({
-		length: quantity > 10 ? quantity : 10,
-	})
-		.fill([])
-		.map((_, i) => (
-			<option key={i} value={i + 1}>
-				{i + 1}
-			</option>
-		))
-);
+const Options = memo(quantity => {
+	const qty = quantity > 10 ? quantity : 10;
+	return [...Array(qty)].map((_, i) => (
+		<option key={i} value={i + 1}>
+			{i + 1}
+		</option>
+	));
+});
 
 const ModalCartList = memo(
 	({ cart, userId, setAppError, onGetUserCart, onOpenModal }) => {
