eeb6710bf25bcf62291c727e47cb8dfdcad1202a
Weiss Bai <whitesgr03@gmail.com>
2023-12-01 13:50:47 +0800

    Refactor ModalCart test

--- src/__test__/modals/ModalCart.test.jsx
+++ src/__test__/modals/ModalCart.test.jsx
@@ -1,37 +1,21 @@
-import { screen, render } from "@testing-library/react";
-
-import { RouterProvider, createMemoryRouter } from "react-router-dom";
-
-import { useCart } from "../../components/RootContext";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { BrowserRouter } from "react-router-dom";
 
 import ModalCart from "../../components/modals/ModalCart";
 
-jest.mock("../../components/RootContext");
-jest.mock("../../firebase-config");
+jest.mock("../../utils/handleUserCart");
 
-describe("Renders ModalCart Component", () => {
-	it("Should return empty class without data", () => {
-		useCart.mockReturnValueOnce([]);
+let mockCart = null;
+let mockActive = null;
 
-		const routes = [
-			{
-				path: "/",
-				element: <ModalCart />,
-			},
-		];
+const mockOnOpenModal = jest.fn();
 
-		const router = createMemoryRouter(routes, {
-			initialEntries: ["/"],
-		});
+describe("ModalCart Component", () => {
+	it("Should add class if active is true", () => {
+		const MockComponent = <div data-testid="content"></div>;
 
-		render(<RouterProvider router={router} />);
-
-		const actual = screen.getByTestId("empty");
-
-		expect(actual).toBeInTheDocument();
-	});
-	it("Should return cart list with data", () => {
-		const mockCartData = [
+		mockCart = [
 			{
 				id: 0,
 				name: "fake",
@@ -40,28 +24,134 @@ describe("Renders ModalCart Component", () => {
 				quantity: 1,
 			},
 		];
+		mockActive = true;
 
-		useCart.mockReturnValueOnce(mockCartData);
+		render(
+			<ModalCart
+				cart={mockCart}
+				active={mockActive}
+				onOpenModal={mockOnOpenModal}
+			>
+				{MockComponent}
+			</ModalCart>,
+			{ wrapper: BrowserRouter }
+		);
 
-		const routes = [
+		const element = screen.getByTestId("cart");
+
+		expect(element).toHaveClass("active");
+	});
+	it("Should not add class if active is false", () => {
+		const MockComponent = <div data-testid="content"></div>;
+
+		mockCart = [
 			{
-				path: "/",
-				element: <ModalCart />,
+				id: 0,
+				name: "fake",
+				url: "../",
+				price: 19.9,
+				quantity: 1,
 			},
 		];
+		mockActive = false;
 
-		const router = createMemoryRouter(routes, {
-			initialEntries: ["/"],
+		render(
+			<ModalCart
+				cart={mockCart}
+				active={mockActive}
+				onOpenModal={mockOnOpenModal}
+			>
+				{MockComponent}
+			</ModalCart>,
+			{ wrapper: BrowserRouter }
+		);
+
+		const element = screen.getByTestId("cart");
+
+		expect(element).not.toHaveClass("active");
+	});
+	it("Should render content if cart is not empty", () => {
+		const MockComponent = <div data-testid="content"></div>;
+
+		mockCart = [
+			{
+				id: 0,
+				name: "fake",
+				url: "../",
+				price: 19.9,
+				quantity: 1,
+			},
+		];
+		mockActive = true;
+
+		render(
+			<ModalCart
+				cart={mockCart}
+				active={mockActive}
+				onOpenModal={mockOnOpenModal}
+			>
+				{MockComponent}
+			</ModalCart>,
+			{ wrapper: BrowserRouter }
+		);
+
+		const element = screen.getByTestId("content");
+
+		expect(element).toBeInTheDocument();
+	});
+	it("Should render paragraph if cart is empty", () => {
+		const MockComponent = <div data-testid="content"></div>;
+
+		mockCart = [];
+		mockActive = true;
+
+		render(
+			<ModalCart
+				cart={mockCart}
+				active={mockActive}
+				onOpenModal={mockOnOpenModal}
+			>
+				{MockComponent}
+			</ModalCart>,
+			{ wrapper: BrowserRouter }
+		);
+
+		const element = screen.getByTestId("empty");
+
+		expect(element).toBeInTheDocument();
+	});
+	it("Should checkout if button is clicked", async () => {
+		const user = userEvent.setup();
+		const MockComponent = <div data-testid="content"></div>;
+
+		mockCart = [
+			{
+				id: 0,
+				name: "fake",
+				url: "../",
+				price: 19.9,
+				quantity: 1,
+			},
+		];
+		mockActive = true;
+
+		render(
+			<ModalCart
+				cart={mockCart}
+				active={mockActive}
+				onOpenModal={mockOnOpenModal}
+			>
+				{MockComponent}
+			</ModalCart>,
+			{ wrapper: BrowserRouter }
+		);
+
+		const element = screen.getByRole("button", {
+			name: "CHECKOUT",
 		});
 
-		render(<RouterProvider router={router} />);
+		await user.click(element);
 
-		expect(screen.getByRole("img")).toHaveAttribute("src", "../");
-
-		expect(screen.getByTestId("quantity")).toHaveValue("1");
-
-		expect(screen.getByText("fake")).toHaveClass("name");
-
-		expect(screen.getByText("Total:")).toHaveClass("price");
+		expect(mockOnOpenModal).toBeCalledTimes(1);
 	});
 });
