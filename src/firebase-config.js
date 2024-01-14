275321fcc88e65e2ed66b1fbcc15091611044979
Weiss Bai <whitesgr03@gmail.com>
2024-01-14 18:54:52 +0800

    Create firebase config

---
+++ src/firebase-config.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+import "firebase/compat/auth";
+
+const firebaseConfig = {
+	// Your web app's Firebase configuration
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const initialFirestore = firebase.firestore;
+const initialStorage = firebase.storage;
+const initialAuth = firebase.auth;
+
+export { initialFirestore, initialStorage, initialAuth };
